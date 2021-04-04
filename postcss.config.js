9c7cda0f7c178b10f24282bc89a78faada061284
Jimmy <jimmystry@gmail.com>
2021-04-04 13:34:40 -0400

    Fixed module issue

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-export const plugins = [
-  // ...
-  require('tailwindcss'),
-  require('autoprefixer'),
-  // ...
-];
+module.exports = {
+  plugins: [
+    require('tailwindcss'), 
+    require('autoprefixer')
+  ],
+};
