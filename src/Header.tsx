25e3f9e6a08205c01f91e672284931e803fb4708
Jimmy <jimmystry@gmail.com>
2021-04-04 19:09:05 -0400

    Links stay underlined when at their route

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,33 +1,75 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 const Header: React.FC = () => {
+  const history = useHistory();
+
+  let initLinks = [true, false, false];
+
+  if (history.location.pathname === '/') initLinks = [true, false, false];
+  if (history.location.pathname === '/discover') initLinks = [false, true, false];
+  if (history.location.pathname === '/galleries') initLinks = [false, false, true];
+
+  const [buttons, setButtons] = useState(initLinks);
+
+  function clickLink(id: number) {
+    let newButtons = [...buttons];
+
+    for (let i = 0; i < 3; i++) {
+      if (i === id) newButtons[i] = true;
+      else newButtons[i] = false;
+    }
+
+    setButtons(newButtons);
+  }
+
   return (
     <div className="Header">
       <h2 className="text-6xl m-4 ml-8 tracking-wider">artsy</h2>
       <ul className="flex flex-row mx-4 space-x-4">
-        <li className="group">
+        <li className="group" onClick={() => clickLink(0)}>
           <Link to="/">about</Link>
-          <div className="nav-link-underline group-hover:border-red-600"></div>
+          <div
+            className={
+              'nav-link-underline ' + (buttons[0]
+                ? 'border-red-600'
+                : 'group-hover:border-red-600')
+            }
+          ></div>
         </li>
 
         <li className="font-extrabold">·</li>
 
-        <li className="group">
+        <li className="group" onClick={() => clickLink(1)}>
           <Link to="/discover">discover</Link>
-          <div className="nav-link-underline group-hover:border-blue-700"></div>
+          <div
+            className={
+              'nav-link-underline ' + (buttons[1]
+                ? 'border-blue-700'
+                : 'group-hover:border-blue-700')
+            }
+          ></div>
         </li>
 
         <li className="font-extrabold">·</li>
 
-        <li className="group">
+        <li className="group" onClick={() => clickLink(2)}>
           <Link to="/galleries">galleries</Link>
-          <div className="nav-link-underline group-hover:border-yellow-300"></div>
+          <div
+            className={
+              'nav-link-underline ' + (buttons[2]
+                ? 'border-yellow-300'
+                : 'group-hover:border-yellow-300')
+            }
+          ></div>
         </li>
 
-        <div className={"cursor-pointer rounded-full border-2 " + 
-        "border-gray-800 hover:border-green-700 transition duration-75 ease-in-out"
-        }>
+        <div
+          className={
+            'cursor-pointer rounded-full border-2 ' +
+            'border-gray-800 hover:border-green-700 transition duration-75 ease-in-out'
+          }
+        >
           <svg
             className="fill-current text-white"
             xmlns="http://www.w3.org/2000/svg"
