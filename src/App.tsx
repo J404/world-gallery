05584b4e8925aab7855e0ada8059c643f10ba36e
Jimmy <jimmystry@gmail.com>
2021-04-04 19:43:05 -0400

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './Header';
 
@@ -9,6 +9,15 @@ function App() {
     <div className="App">
       <Router>
         <Header></Header>
+        <Switch>
+          <Route path="/"></Route>
+
+          <Route path="/discover">
+            
+          </Route>
+
+          <Route path="/galleries"></Route>
+        </Switch>
       </Router>
     </div>
   );
