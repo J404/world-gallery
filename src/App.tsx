98b8dcab176b70a0252d2374cf2c6a4f8dd95b39
Jimmy <jimmystry@gmail.com>
2021-04-04 20:33:47 -0400

    Leaflet js map is working in basic form. TODO: once production site is deployed, set domain as trusted in Mapbox and make change to actual API key (ONLY ONCE THE SINGLE TRUSTED DOMAIN IS SET) current API env key is temp solution and wont work in production

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './Header';
+import DiscoverMap from './Map';
 
 function App() {
   return (
@@ -10,13 +11,13 @@ function App() {
       <Router>
         <Header></Header>
         <Switch>
-          <Route path="/"></Route>
-
           <Route path="/discover">
-            
+            <DiscoverMap></DiscoverMap>
           </Route>
 
           <Route path="/galleries"></Route>
+
+          <Route path="/"></Route>
         </Switch>
       </Router>
     </div>
--- src/Map.tsx
+++ src/Map.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-const Map: React.FC = () => {
-  const discovermap = L.map('discover-map').setView([51.505, -0.09], 13);
-  L.tileLayer(
-    `https://api.mapbox.com/styles/v1/J404/tiles/1/51.505/-0.09?access_token=${process.env.API_KEY}`
-  );
+import * as L from 'leaflet';
+
+const DiscoverMap: React.FC = () => {
+  const didMount = useRef(false);
+
+  useEffect(() => { 
+    let discovermap: L.Map = {} as unknown as L.Map;
+
+    if (!didMount.current) {  
+      discovermap = L.map('discover-map').setView(
+        [51.505, -0.09],
+        13
+      );
+
+      didMount.current = true;
+    } else discovermap = L.map('discover-map');
+      
+    L.tileLayer(
+      `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`,
+      {
+        attribution: 'Mapbox and stuff',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: process.env.REACT_APP_API_KEY,
+      }
+    ).addTo(discovermap);
+  });
 
   return (
-    <div className='Map'>
-      <div id="discover-map" style={{height: '180px'}}></div>
+    <div className="DiscoverMap">
+      <p>Map component!</p>
+      <div id="discover-map" className='w-3/4 float-right mr-12' style={{height: '80vh',}}
+      ></div>
     </div>
   );
-}
-export default Map;
+};
+export default DiscoverMap;
