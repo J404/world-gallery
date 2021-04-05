6e5016dc59f943965c0a62f48a00ebe7d3a17db0
Jimmy <jimmystry@gmail.com>
2021-04-04 21:47:18 -0400

    Example location marker

--- src/Map.tsx
+++ src/Map.tsx
@@ -3,16 +3,24 @@ import React, { useEffect, useRef } from 'react';
 import * as L from 'leaflet';
 
 const DiscoverMap: React.FC = () => {
+  let discovermap = useRef<L.Map>(({} as unknown) as L.Map);
   const didMount = useRef(false);
 
-  useEffect(() => {
-    let discovermap: L.Map = ({} as unknown) as L.Map;
+  const addMarker = (coords: [number, number], title: string, link: string) => {
+    const marker = L.marker(coords).addTo(discovermap.current);
+    marker.bindPopup(`
+    <b>${title}</b>
+    <br>
+    <a href="${link}">Visit</a>
+    `);
+  };
 
+  useEffect(() => {
     // Only recreate if the component isn't mounted - not on reload
     if (!didMount.current) {
       didMount.current = true;
 
-      discovermap = L.map('discover-map').setView([51.505, -0.09], 13);
+      discovermap.current = L.map('discover-map').setView([51.505, -0.09], 13);
 
       L.tileLayer(
         `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`,
@@ -24,21 +32,21 @@ const DiscoverMap: React.FC = () => {
           zoomOffset: -1,
           accessToken: process.env.REACT_APP_API_KEY,
         }
-      ).addTo(discovermap);
+      ).addTo(discovermap.current);
 
       // Go to broswer's location if they allow
       if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(pos => {
+        navigator.geolocation.getCurrentPosition((pos) => {
           const lat = pos.coords.latitude;
           const lon = pos.coords.longitude;
           const coords: [number, number] = [lat, lon];
 
-          discovermap.panTo(coords);
+          discovermap.current.panTo(coords);
+
+          addMarker(coords, 'Example', 'http://localhost:3000/galleries');
         });
       }
     }
-
-    
   });
 
   return (
