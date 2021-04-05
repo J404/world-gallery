898e9239f40a6a20cc0ba73883e6fd513d8af693
Jimmy <jimmystry@gmail.com>
2021-04-04 23:44:28 -0400

    Wall of text - subject to change

--- src/Discover.tsx
+++ src/Discover.tsx
@@ -9,8 +9,30 @@ const Discover: React.FC = () => {
         <div className='group w-2/5 mx-auto'>
           <h2 className='text-6xl tracking-wide'>
             Discover
-            <div className='nav-link-underline border-2 group-hover:border-blue-700'></div>
+            <div className='nav-link-underline border-2 border-blue-700 mt-2'
+            ></div>
           </h2>
+        </div>
+        <div id='description'
+        className='text-lg mt-5 w-3/4 mx-auto space-y-6'>
+          <p>
+            Are you looking for local or international art from 
+            artists with little exposure or publicity?
+          </p>
+          <p>
+            Our discovery map makes it easy to find art from creators
+            near you, abroad, or any place in between.
+          </p>
+          <p>
+            Artists are freed from traditional requirements of publishing, and
+            you are free to browse at your leisure.
+          </p>
+          <p>
+            Prices are affordable and sensical.
+          </p>
+          <p>
+            Discover the next big thing!
+          </p>
         </div>
       </div>
       <div className='col-span-3'>
@@ -19,4 +41,5 @@ const Discover: React.FC = () => {
     </div>
   );
 };
+
 export default Discover;
