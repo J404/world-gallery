2db2a55b961d6d8c6221b93db7a2013e81da6091
Jimmy <jimmystry@gmail.com>
2021-04-04 21:00:32 -0400

    Messed up geolocation

--- src/Map.tsx
+++ src/Map.tsx
@@ -5,37 +5,71 @@ import * as L from 'leaflet';
 const DiscoverMap: React.FC = () => {
   const didMount = useRef(false);
 
-  useEffect(() => { 
-    let discovermap: L.Map = {} as unknown as L.Map;
+  async function getCoords() {
+    let coords: [number, number] = [0, 0];
+    const geo = navigator.geolocation;
 
-    if (!didMount.current) {  
-      discovermap = L.map('discover-map').setView(
-        [51.505, -0.09],
-        13
+    console.log(geo);
+
+    if (geo) {
+      const location = geo.getCurrentPosition(
+        (position) => {
+          coords = [position.coords.latitude, position.coords.longitude];
+          console.log(coords);
+          return coords;
+        },
+        () => {
+          console.error('Could not fetch location of browser');
+          return null;
+        }
       );
+    } else {
+      return null;
+    }
+  }
 
-      didMount.current = true;
-    } else discovermap = L.map('discover-map');
-      
-    L.tileLayer(
-      `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`,
-      {
-        attribution: 'Mapbox and stuff',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: process.env.REACT_APP_API_KEY,
-      }
-    ).addTo(discovermap);
+  useEffect(() => {
+    async function makeMap() {
+      let coords: [number, number] = [51.505, -0.09];
+      const browserCoords = await getCoords();
+
+      if (browserCoords) coords = browserCoords;
+
+      let discovermap: L.Map = ({} as unknown) as L.Map;
+
+      console.log(coords);
+
+      if (!didMount.current) {
+        discovermap = L.map('discover-map').setView(coords, 13);
+        didMount.current = true;
+      } else discovermap = L.map('discover-map');
+
+      L.tileLayer(
+        `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`,
+        {
+          attribution: 'Mapbox and stuff',
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: process.env.REACT_APP_API_KEY,
+        }
+      ).addTo(discovermap);
+    }
+
+    makeMap();
   });
 
   return (
     <div className="DiscoverMap">
       <p>Map component!</p>
-      <div id="discover-map" className='w-3/4 float-right mr-12' style={{height: '80vh',}}
+      <div
+        id="discover-map"
+        className="w-3/4 float-right mr-12"
+        style={{ height: '80vh' }}
       ></div>
     </div>
   );
 };
+
 export default DiscoverMap;
