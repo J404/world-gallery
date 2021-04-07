18ba7c158c362d374ceaeace01ab6d49d5cd2d1c
Jimmy <jimmystry@gmail.com>
2021-04-06 20:40:53 -0400

    Gallery has two different views depending on url parameter

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -1,15 +1,52 @@
 import React from 'react';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams,
+} from 'react-router-dom';
+
 import ArtPiece from './ArtPiece';
 
 const Gallery: React.FC = () => {
+  const GalleryView: React.FC = () => {
+    const { artist } = useParams<{ artist: string }>();
+
+    return (
+      <div className='GalleryView grid grid-cols-4'>
+        <div className='text-center'>
+          <h2 className='text-4xl'>
+            {artist}'s Gallery
+          </h2>
+        </div>
+        <div className='flex flex-row col-span-3'>
+          <ArtPiece
+            imageurl='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hausziege_04.jpg/1200px-Hausziege_04.jpg'
+            artist='John Doe'
+            title='Goat'
+          ></ArtPiece>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className='Gallery'>
-      gallery
-      <ArtPiece imageurl='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hausziege_04.jpg/1200px-Hausziege_04.jpg'
-      artist='John Doe' title='Goat'></ArtPiece>
+      <Router>
+        <Switch>
+          <Route path='/galleries/:artist'
+          children={
+            <GalleryView></GalleryView>
+          }></Route>
+
+          <Route path='/galleries'>
+            <p>helakedf</p>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default Gallery;
