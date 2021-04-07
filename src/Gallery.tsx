eccbda4d6a704b05273d9e3a0041c0eac64a525c
Jimmy <jimmystry@gmail.com>
2021-04-06 21:36:17 -0400

    Semi-works, will attempt to fix small bug

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   useParams,
+  useLocation,
+  useHistory,
 } from 'react-router-dom';
 
 import ArtPiece from './ArtPiece';
 
 const Gallery: React.FC = () => {
+  const history = useHistory();
+
   const GalleryView: React.FC = () => {
     const { artist } = useParams<{ artist: string }>();
 
     return (
       <div className='GalleryView grid grid-cols-4'>
-        <div className='text-center w-fit-content mx-auto'>
-          <h2 className='text-4xl w-fit-content mx-auto'>
-            {artist}'s Gallery
-          </h2>
-          <div className='border-2 border-yellow-300 rounded'
-          ></div>
+        <div className='text-center'>
+          <div className='w-fit-content mx-auto'>
+            <h2 className='text-4xl w-fit-content mx-auto'>
+              {artist}'s Gallery
+            </h2>
+            <div className='border-2 border-yellow-300 rounded'
+            ></div>
+          </div>
+          <div>
+            <p className='text-2xl my-3'
+            >"Example placeholder quote."</p>
+            <p className='text-lg my-4'>
+              Example of a description
+            </p>
+          </div>
         </div>
+
         <div className='flex flex-row col-span-3'>
           <ArtPiece
             imageurl='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hausziege_04.jpg/1200px-Hausziege_04.jpg'
@@ -43,7 +57,9 @@ const Gallery: React.FC = () => {
           }></Route>
 
           <Route path='/galleries'>
-            <p>helakedf</p>
+            <div>
+
+            </div>
           </Route>
         </Switch>
       </Router>
