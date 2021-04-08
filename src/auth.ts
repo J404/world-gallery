68794bacacdddf715c67702ec9e21692ebab8453
Jimmy <jimmystry@gmail.com>
2021-04-08 17:31:56 -0400

    Moved firebase config to gitignored file

--- src/auth.ts
+++ src/auth.ts
@@ -1,19 +1,10 @@
 import firebase from 'firebase/app';
+import initFirebase from './firebaseconfig';
 
 import 'firebase/auth';
 
 // Initialize firebase w/ config
-const firebaseConfig = {
-  apiKey: 'AIzaSyDRctPNHhmgT4xCtX1MsxNwvbG6bH3bD9A',
-  authDomain: 'worldgallery-22545.firebaseapp.com',
-  projectId: 'worldgallery-22545',
-  storageBucket: 'worldgallery-22545.appspot.com',
-  messagingSenderId: '657905455305',
-  appId: '1:657905455305:web:405f832012388ff749bd18',
-  measurementId: 'G-7S291E8H96',
-};
-
-firebase.initializeApp(firebaseConfig);
+initFirebase();
 
 const apiRoute = 'https://us-central1-worldgallery-22545.cloudfunctions.net/api';
 
